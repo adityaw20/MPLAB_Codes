3b0a9c0fc42728869b11f265862fc7562e4f0117
Aditya Walzade <84090476+adityaw20@users.noreply.github.com>
2025-08-17 20:03:58 +0530

    c and hex files

---
+++ led.c
@@ -0,0 +1,30 @@
+#include<p18f4580.h>
+
+#define led PORTCbits.RC0
+void delay(int a);
+
+int main()
+{
+
+TRISCbits.RC0=0;
+
+while(1)
+{
+led=1;
+delay(300);
+led=0;
+delay(300);
+}
+
+}
+
+void delay(int a)
+{
+int i,j;
+for(i=0;i<a;i++)
+{
+for(j=0;j<a;j++)
+{
+}
+}
+}
